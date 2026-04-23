c8adff21e1cb5eedc347a287ebef65a68be96db8
cc <976181377@qq.com>
2026-04-23 16:09:01 +0800

    fix:调整静态文件路径

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.4", "localhost", "127.0.0.1"],
   output: 'export',
-
-  // 禁用默认的图片优化 API（因为 GitHub Pages 不支持动态图片优化服务器）
-  images: {
-    unoptimized: true,
-  },
   basePath: '/nanjinghexi_school',
+  assetPrefix: '/nanjinghexi_school/',
 };
 
 export default nextConfig;
