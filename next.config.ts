45df8f39f5cc7ae0f89096a779a3d8fde8dcaffa
cc <976181377@qq.com>
2026-04-23 17:36:48 +0800

    fix: 修复deploy

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.4", "localhost", "127.0.0.1"],
-  basePath: '/nanjinghexi_school',
+  basePath:'/nanjinghexi_school',
   output: "export",
   images: {
     unoptimized: true,
   },
   env: {
-    NEXT_PUBLIC_BASE_PATH: '/nanjinghexi_school',
+    NEXT_PUBLIC_BASE_PATH: isProd ? '/nanjinghexi_school' : '',
   },
 };
 
